d4716eb60c0607d2179186caccd90c6bfe70cbf9
Ollie B <oliver.burge2014@gmail.com>
2018-09-21 08:35:54 +0100

    Implemented initial file uploading system via carrierwave for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_103507) do
+ActiveRecord::Schema.define(version: 2018_09_21_070947) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_09_17_103507) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "image"
   end
 
   create_table "users", force: :cascade do |t|
